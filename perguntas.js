55610264c29b861525f40b0b40f7498644e7e68b
Mh_Kemuri <162151179+felipeflauzino2008@users.noreply.github.com>
2025-06-23 12:26:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'programação',
+    'o que e javascript?',
+    'o javascript e uma linguagem de programaçao'
+)
+criaCartao(
+    'geografia',
+    'qual a capital do parana?',
+    'a capital do parana e curitiba'
+)
+criaCartao(
+    'jogo',
+    'qual o jogo mais vendido da historia?',
+    'o jogo mais vendido da historia e minecraft'
+)
